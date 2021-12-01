4845b1eab224637c173925f03b062a5924df6e3c
Matti Lupari <matti.lupari@gmail.com>
2021-12-01 07:19:34 +0200

    Day 01

--- src/test/scala/Test01.scala
+++ src/test/scala/Test01.scala
@@ -4,8 +4,7 @@ import challenge.Day01
 class Test01 extends flatspec.AnyFlatSpec with matchers.should.Matchers {
 
   it should "calculate correct result" in {
-    // Day01.partOne() should be(1006176)
-    // Day01.partTwo() should be(199132160)
+    Day01.partOne() should be(1184)
+    Day01.partTwo() should be(1158)
   }
 }
-
