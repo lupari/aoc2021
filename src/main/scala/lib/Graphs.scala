b96398fc2dfaf751d0e788b61147615d670c2e04
Matti Lupari <matti.lupari@arcusys.fi>
2021-12-02 13:48:04 +0200

    Use Scala 3 syntax for old implicits

--- src/main/scala/lib/Graphs.scala
+++ src/main/scala/lib/Graphs.scala
@@ -7,13 +7,11 @@ object Graphs {
 
   object DFS {
     def apply[A](start: A)(nf: A => Iterable[A]): Iterable[A] = {
-      def _dfs(s: A, seen: Iterable[A]): Iterable[A] = {
-        if (seen.iterator.contains(s)) seen
-        else {
+      def _dfs(s: A, seen: Iterable[A]): Iterable[A] =
+        if seen.iterator.contains(s) then seen
+        else
           val neighbors = nf(s).filterNot(seen.iterator.contains)
           neighbors.foldLeft(Iterable(s) ++ seen)((b, a) => _dfs(a, b))
-        }
-      }
 
       _dfs(start, Nil)
     }
@@ -22,13 +20,13 @@ object Graphs {
   object BFS {
     def apply[A](start: A)(nf: A => Iterable[A]): Map[A, Int] = {
       @tailrec
-      def _bfs(seen: Map[A, Int], unseen: Map[A, Int]): Map[A, Int] = {
-        val neighbors = for { (node, cost) <- unseen; newNode <- nf(node) } yield
-          newNode -> (cost + 1)
+      def _bfs(seen: Map[A, Int], unseen: Map[A, Int]): Map[A, Int] =
+        val neighbors = for {
+          (node, cost) <- unseen; newNode <- nf(node)
+        } yield newNode -> (cost + 1)
         val seen2   = seen ++ unseen
         val unseen2 = neighbors.filterNot(n => seen.contains(n._1))
-        if (unseen2.isEmpty) seen2 else _bfs(seen2, unseen2)
-      }
+        if unseen2.isEmpty then seen2 else _bfs(seen2, unseen2)
 
       _bfs(Map.empty, Map(start -> 0))
     }
@@ -38,22 +36,19 @@ object Graphs {
     def apply[A](start: A, goal: A)(cf: (A, A) => Int)(nf: A => Iterable[A]): Map[A, Int] = {
       val open  = mutable.ListBuffer[(A, Int)]((start, 0))
       val costs = mutable.Map[A, Int](start -> 0)
-      while (open.nonEmpty) {
+      while open.nonEmpty do
         val current = open.minBy(_._2)
         open -= current
-        if (current._1 == goal) open.clear()
-        else {
+        if current._1 == goal then open.clear()
+        else
           val newCost   = costs(current._1) + 1
           val neighbors = nf(current._1)
-          for (next <- neighbors) {
-            if (!costs.contains(next) || newCost < costs(next)) {
+          for next <- neighbors do
+            if (!costs.contains(next) || newCost < costs(next))
               costs(next) = newCost
               val priority = newCost + cf(goal, next)
               open += ((next, priority))
-            }
-          }
-        }
-      }
+
       costs.toMap
     }
   }
--- src/main/scala/lib/GridExtensions.scala
+++ src/main/scala/lib/GridExtensions.scala
@@ -5,30 +5,28 @@ import lib.Points.Point
 import scala.annotation.tailrec
 import scala.reflect.ClassTag
 
-object GridImplicits {
+object GridExtensions {
 
   type Grid[A] = Map[Point, A]
 
-  implicit class GridCanvas[A](grid: Grid[A]) {
-    def canvas(default: A)(cf: A => A)(implicit classTag: ClassTag[A]): Array[Array[A]] = {
+  extension [A](grid: Grid[A]) {
+    def canvas(default: A)(cf: A => A)(using classTag: ClassTag[A]): Array[Array[A]] = {
       val (x, y) = (grid.keys.maxBy(_.x).x, grid.keys.maxBy(_.y).y)
       val canvas = Array.tabulate(y + 1, x + 1)((_, _) => default)
-      for { p <- grid } yield canvas(p._1.y)(p._1.x) = cf(p._2)
+      for p <- grid yield canvas(p._1.y)(p._1.x) = cf(p._2)
       canvas
     }
   }
 
-  implicit class GridInput(input: Seq[Char]) {
+  extension (input: Seq[Char])
     def toGrid: Grid[Char] = {
       @tailrec
       def _toGrid(xs: Seq[Char], acc: Grid[Char], current: Point): Grid[Char] =
-        xs match {
+        xs match
           case h :: t if h == '\n' => _toGrid(t, acc, Point(0, current.y + 1))
-          case h :: t              => _toGrid(t, acc.updated(current, h), Point(current.x + 1, current.y))
-          case _                   => acc
-        }
+          case h :: t => _toGrid(t, acc.updated(current, h), Point(current.x + 1, current.y))
+          case _      => acc
       _toGrid(input, Map.empty, Point(0, 0))
     }
-  }
 
 }
