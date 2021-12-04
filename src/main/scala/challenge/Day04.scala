3eec806bb601b9a0e9297e90edaaa93e168f1b0a
Matti Lupari <matti.lupari@gmail.com>
2021-12-04 23:06:02 +0200

    Day 04 - model board as lists of sets

--- src/main/scala/challenge/Day04.scala
+++ src/main/scala/challenge/Day04.scala
@@ -2,29 +2,26 @@ package challenge
 
 import scala.io.Source
 import scala.annotation.tailrec
-import lib.GridExtensions._
-import lib.Points.Point
 
 object Day04 {
 
-  case class Square(value: Int, marked: Boolean = false)
-  case class Player(board: Grid[Square]):
-    def mark(n: Int): Player = board.find(_._2.value == n) match
-      case Some((p, s)) => Player(board.updated(p, s.copy(marked = true)))
-      case None         => this
-    def hasBingo(): Boolean =
-      def checkRow(i: Int) = board.filter(_._1.y == i).forall(_._2.marked == true)
-      def checkCol(i: Int) = board.filter(_._1.x == i).forall(_._2.marked == true)
-      (0 to 4).map(checkRow).exists(_ == true) || (0 to 4).map(checkCol).exists(_ == true)
-    def score(): Int = board.values.filter(_.marked == false).map(_.value).sum
+  case class Player(board: List[Set[Int]]):
+    def mark(n: Int): Player = copy(board = board.map(_ - n))
+    val score: Int           = board.flatten.toSet.sum
+    val hasBingo: Boolean    = board.exists(_.isEmpty)
 
-  def toBingoSheet(lines: List[List[Int]]): Grid[Square] = {
-    for i <- 0 to 4; j <- 0 to 4
-    yield (
-      Point(j, i),
-      Square(lines(i)(j))
-    )
-  }.toMap
+  def toPlayer(lines: List[List[Int]]): Player = Player((lines ++ lines.transpose).map(_.toSet))
+
+  def findScore()(fn: (List[Player]) => (Option[Player], List[Player])): Option[Int] =
+    @tailrec
+    def helper(xs: List[Int], ps: List[Player]): Option[Int] = xs match
+      case Nil => None
+      case h :: t =>
+        fn(ps.map(_.mark(h))) match
+          case (None, ps2)  => helper(t, ps2)
+          case (Some(p), _) => Option(p.score * h)
+
+    helper(numbers, players)
 
   val input: List[String] = Source.fromResource("day04.txt").getLines().toList
   val numbers: List[Int]  = input.head.split(",").map(_.toInt).toList
@@ -33,26 +30,14 @@ object Day04 {
       .drop(2)
       .grouped(6)
       .map(_.filter(_.nonEmpty))
-      .map(ls => toBingoSheet(ls.map(_.trim.split(" +").toList.map(_.toInt))))
+      .map(ls => toPlayer(ls.map(_.trim.split(" +").toList.map(_.toInt))))
       .toList
-      .map(Player.apply)
 
-  def findScore()(fn: (List[Player]) => (Option[Player], List[Player])): Option[Int] =
-    @tailrec
-    def helper(xs: List[Int], ps: List[Player]): Option[Int] = xs match
-      case Nil => None
-      case h :: t =>
-        fn(ps.map(p => p.mark(h))) match
-          case (None, ps2)  => helper(t, ps2)
-          case (Some(w), _) => Option(w.score() * h)
-
-    helper(numbers, players)
-
-  def partOne(): Int = findScore()(ps => (ps.find(_.hasBingo()), ps)).get
+  def partOne(): Int = findScore()(ps => (ps.find(_.hasBingo), ps)).get
   def partTwo(): Int =
     def findLoser = (ps: List[Player]) =>
-      val losers = ps.filterNot(_.hasBingo())
-      if (losers.isEmpty && ps.length == 1) (Option(ps.head), Nil) else (None, losers)
+      val losers = ps.filterNot(_.hasBingo)
+      if losers.isEmpty && ps.length == 1 then (Option(ps.head), Nil) else (None, losers)
 
     findScore()(findLoser).get
 
