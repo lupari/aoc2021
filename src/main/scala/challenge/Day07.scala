48034b681f9ff5a8ad7b724f65346b397dc784b0
Matti Lupari <matti.lupari@arcusys.fi>
2021-12-07 12:00:06 +0200

    Day 3 - use median and mean

--- src/main/scala/challenge/Day07.scala
+++ src/main/scala/challenge/Day07.scala
@@ -1,16 +1,19 @@
 package challenge
 
 import scala.io.Source
-import lib.Math.arithmeticSum
+import lib.Math._
 
 object Day07 {
 
-  def cost()(fn: (Int, Int) => Int): Int =
-    (crabs.min to crabs.max).map(i => crabs.map(fn(i, _)).sum).min
+  def fuelCost(n: Int)(costFn: (Int, Int) => Int): Int = crabs.map(costFn(n, _)).sum
 
   val crabs: List[Int] = Source.fromResource("day07.txt").mkString.split(",").map(_.toInt).toList
 
-  def partOne(): Int = cost()((a, b) => (a - b).abs)
-  def partTwo(): Int = cost()(arithmeticSum)
+  def partOne(): Int = fuelCost(median(crabs))((a, b) => (a - b).abs)
+  def partTwo(): Int =
+    val avg = mean(crabs)
+    Seq(avg.floor.toInt, avg.ceil.toInt)
+      .map(fuelCost(_)((a, b) => arithmeticSeries(a, b, normalized = true)))
+      .min
 
 }
--- src/main/scala/lib/Math.scala
+++ src/main/scala/lib/Math.scala
@@ -2,6 +2,8 @@ package lib
 
 object Math {
 
+  def mean(seq: Seq[Int]): Double = seq.sum / seq.length.toDouble
+
   def median(seq: Seq[Int]): Int =
     val sorted = seq.sorted
     if seq.size % 2 == 1 then sorted(sorted.size / 2)
@@ -9,8 +11,10 @@ object Math {
       val (up, down) = sorted.splitAt(seq.size / 2)
       (up.last + down.head) / 2
 
-  def arithmeticSum(a: Int, b: Int): Int =
-    val dist = (a - b).abs
-    dist + (dist * (dist - 1) / 2)
+  def arithmeticSeries(a: Int, b: Int, step: Int = 1, normalized: Boolean = false): Int =
+    val dist     = (a - b).abs
+    val (a1, an) = if normalized then (0, dist) else (a, b)
+    val n        = dist / step + 1
+    n * (a1 + an) / 2
 
 }
