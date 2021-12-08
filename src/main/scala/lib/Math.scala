584b769d59cc4b8ace0225002d5e83410682c8ac
Matti Lupari <matti.lupari@gmail.com>
2021-12-08 14:48:43 +0200

    Day 8

--- src/main/scala/lib/Math.scala
+++ src/main/scala/lib/Math.scala
@@ -6,7 +6,7 @@ object Math {
 
   def median(seq: Seq[Int]): Int =
     val sorted = seq.sorted
-    if seq.size % 2 == 1 then sorted(sorted.size / 2)
+    if seq.size % 2 == 1 then sorted(seq.size / 2)
     else
       val (up, down) = sorted.splitAt(seq.size / 2)
       (up.last + down.head) / 2
